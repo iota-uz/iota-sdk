8a97951bbb4fda5579238282d47fbd45849ca6a4
Diyor Khaydarov <diyor28july@gmail.com>
2024-06-01 21:01:52 +0500

    major refactoring

--- pkg/server/graphql/routes/bichat/chatfuncs/types.go
+++
@@ -1,8 +0,0 @@
-package chatfuncs
-
-type ChatFunctionDefinition interface {
-	Name() string
-	Description() string
-	Arguments() map[string]interface{}
-	Execute(args map[string]interface{}) (string, error)
-}
--- pkg/server/graphql/routes/types.go
+++
@@ -1,8 +0,0 @@
-package routes
-
-import (
-	"github.com/graphql-go/graphql"
-	"gorm.io/gorm"
-)
-
-type GraphQLConstructor func(db *gorm.DB) []*graphql.Field
