ba60c920c265a893568dbaa717fd29d8ad07e364
Diyor Khaydarov <diyor28july@gmail.com>
2024-06-24 20:15:59 +0500

    new event publisher

--- internal/infrastracture/event/event_publisher.go
+++ internal/infrastracture/event/event_publisher.go
@@ -1,8 +1,14 @@
 package event
 
+import "reflect"
+
+type Event interface {
+	Name() string
+	Data() interface{}
+}
+
 type Subscriber struct {
-	Event   string
-	Handler func(data interface{})
+	Handler interface{}
 }
 
 type Publisher struct {
@@ -13,14 +19,43 @@ func NewEventPublisher() *Publisher {
 	return &Publisher{}
 }
 
-func (p *Publisher) Publish(event string, data interface{}) {
-	for _, subscriber := range p.Subscribers {
-		if subscriber.Event == event {
-			go subscriber.Handler(data)
+func (p *Publisher) checkSignature(handler interface{}, args []interface{}) bool {
+	t := reflect.TypeOf(handler)
+	if t.Kind() != reflect.Func {
+		return false
+	}
+	if t.NumIn() != len(args) {
+		return false
+	}
+	for i, arg := range args {
+		if t.In(i) != reflect.TypeOf(arg) {
+			return false
 		}
+	}
+	return true
+}
+
+func (p *Publisher) Publish(args ...interface{}) {
+	for _, subscriber := range p.Subscribers {
+		v := reflect.ValueOf(subscriber.Handler)
+		if !p.checkSignature(subscriber.Handler, args) {
+			continue
+		}
+		in := make([]reflect.Value, len(args))
+		for i, arg := range args {
+			in[i] = reflect.ValueOf(arg)
+		}
+		v.Call(in)
 	}
 }
 
-func (p *Publisher) Subscribe(event string, handler func(data interface{})) {
-	p.Subscribers = append(p.Subscribers, Subscriber{Event: event, Handler: handler})
+func (p *Publisher) Subscribe(handler interface{}) {
+	t := reflect.TypeOf(handler)
+	if t.Kind() != reflect.Func {
+		panic("handler must be a function")
+	}
+	p.Subscribers = append(
+		p.Subscribers,
+		Subscriber{Handler: handler},
+	)
 }
