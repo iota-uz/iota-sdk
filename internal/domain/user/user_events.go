73062d5eaaf84b58098a322acfa429c8295fe859
Diyor Khaydarov <diyor28july@gmail.com>
2024-06-26 08:52:27 +0500

    event publisher fixes

--- internal/domain/user/user_events.go
+++ internal/domain/user/user_events.go
@@ -3,21 +3,21 @@ package user
 import "github.com/iota-agency/iota-erp/internal/domain/session"
 
 type Created struct {
-	User    *User
+	Sender  *User
 	Session *session.Session
 	Data    *User
 	Result  *User
 }
 
 type Updated struct {
-	User    *User
+	Sender  *User
 	Session *session.Session
 	Data    *User
 	Result  *User
 }
 
 type Deleted struct {
-	User    *User
+	Sender  *User
 	Session *session.Session
 	Result  *User
 }
