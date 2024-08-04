f19029eb2e26b229ba1a0e12c9c407376cb83be1
jcbbb <avaz28082000@gmail.com>
2024-08-04 22:39:43 +0500

    improvements

--- internal/presentation/tailwind.config.js
+++ internal/presentation/tailwind.config.js
@@ -11,21 +11,21 @@ module.exports = {
           DEFAULT: "oklch(var(--black))"
         },
         primary: {
-          500: "oklch(var(--primary-500))",
-          600: "oklch(var(--primary-600))",
-          700: "oklch(var(--primary-700))",
+          500: "oklch(var(--primary-500) / <alpha-value>)",
+          600: "oklch(var(--primary-600) / <alpha-value>)",
+          700: "oklch(var(--primary-700) / <alpha-value>)",
         },
         gray: {
-          100: "oklch(var(--gray-100))",
-          200: "oklch(var(--gray-200))",
-          300: "oklch(var(--gray-300))",
-          400: "oklch(var(--gray-400))",
-          500: "oklch(var(--gray-500))",
+          100: "oklch(var(--gray-100) / <alpha-value>)",
+          200: "oklch(var(--gray-200) / <alpha-value>)",
+          300: "oklch(var(--gray-300) / <alpha-value>)",
+          400: "oklch(var(--gray-400) / <alpha-value>)",
+          500: "oklch(var(--gray-500) / <alpha-value>)",
         },
         red: {
-          100: "oklch(var(--red-100))",
-          200: "oklch(var(--red-200))",
-          500: "oklch(var(--red-500))",
+          100: "oklch(var(--red-100) / <alpha-value>)",
+          200: "oklch(var(--red-200) / <alpha-value>)",
+          500: "oklch(var(--red-500) / <alpha-value>)",
         }
       }
     },
