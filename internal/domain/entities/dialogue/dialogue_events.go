16eac46b2e4b0e374c8f29d2ca27eb3ca06bca2b
Diyor Khaydarov <diyor28july@gmail.com>
2024-10-06 17:28:02 +0500

    fix: dialogue_service linting errors

--- internal/domain/entities/dialogue/dialogue_events.go
+++ internal/domain/entities/dialogue/dialogue_events.go
@@ -1,26 +1,75 @@
 package dialogue
 
 import (
+	"context"
 	"github.com/iota-agency/iota-erp/internal/domain/entities/session"
 	"github.com/iota-agency/iota-erp/internal/domain/entities/user"
+	"github.com/iota-agency/iota-erp/pkg/composables"
 )
 
-type Created struct {
-	Data    *Dialogue
-	Result  *Dialogue
-	Sender  *user.User
-	Session *session.Session
+func NewCreatedEvent(ctx context.Context, data Dialogue) (*CreatedEvent, error) {
+	sender, err := composables.UseUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+	sess, err := composables.UseSession(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &CreatedEvent{
+		Sender:  *sender,
+		Session: *sess,
+		Data:    data,
+	}, nil
 }
 
-type Updated struct {
-	Data    *Dialogue
-	Result  *Dialogue
-	Sender  *user.User
-	Session *session.Session
+func NewUpdatedEvent(ctx context.Context, data Dialogue) (*UpdatedEvent, error) {
+	sender, err := composables.UseUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+	sess, err := composables.UseSession(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &UpdatedEvent{
+		Sender:  *sender,
+		Session: *sess,
+		Data:    data,
+	}, nil
 }
 
-type Deleted struct {
-	Result  *Dialogue
-	Sender  *user.User
-	Session *session.Session
+func NewDeletedEvent(ctx context.Context) (*DeletedEvent, error) {
+	sender, err := composables.UseUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+	sess, err := composables.UseSession(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &DeletedEvent{
+		Sender:  *sender,
+		Session: *sess,
+	}, nil
+}
+
+type CreatedEvent struct {
+	Data    Dialogue
+	Result  Dialogue
+	Sender  user.User
+	Session session.Session
+}
+
+type UpdatedEvent struct {
+	Data    Dialogue
+	Result  Dialogue
+	Sender  user.User
+	Session session.Session
+}
+
+type DeletedEvent struct {
+	Result  Dialogue
+	Sender  user.User
+	Session session.Session
 }
--- internal/interfaces/graph/bi_chat.resolvers.go
+++ internal/interfaces/graph/bi_chat.resolvers.go
@@ -125,7 +125,7 @@ func (r *queryResolver) Prompts(ctx context.Context, offset int, limit int, sort
 // DialogueCreated is the resolver for the dialogueCreated field.
 func (r *subscriptionResolver) DialogueCreated(ctx context.Context) (<-chan *model.Dialogue, error) {
 	ch := make(chan *model.Dialogue)
-	r.app.EventPublisher.Subscribe(func(evt *dialogue.Created) {
+	r.app.EventPublisher.Subscribe(func(evt *dialogue.CreatedEvent) {
 		res, err := evt.Result.ToGraph()
 		if err == nil {
 			ch <- res
@@ -137,7 +137,7 @@ func (r *subscriptionResolver) DialogueCreated(ctx context.Context) (<-chan *mod
 // DialogueUpdated is the resolver for the dialogueUpdated field.
 func (r *subscriptionResolver) DialogueUpdated(ctx context.Context) (<-chan *model.Dialogue, error) {
 	ch := make(chan *model.Dialogue)
-	r.app.EventPublisher.Subscribe(func(evt *dialogue.Updated) {
+	r.app.EventPublisher.Subscribe(func(evt *dialogue.UpdatedEvent) {
 		res, err := evt.Result.ToGraph()
 		if err == nil {
 			ch <- res
@@ -149,7 +149,7 @@ func (r *subscriptionResolver) DialogueUpdated(ctx context.Context) (<-chan *mod
 // DialogueDeleted is the resolver for the dialogueDeleted field.
 func (r *subscriptionResolver) DialogueDeleted(ctx context.Context) (<-chan *model.Dialogue, error) {
 	ch := make(chan *model.Dialogue)
-	r.app.EventPublisher.Subscribe(func(evt *dialogue.Deleted) {
+	r.app.EventPublisher.Subscribe(func(evt *dialogue.DeletedEvent) {
 		res, err := evt.Result.ToGraph()
 		if err == nil {
 			ch <- res
