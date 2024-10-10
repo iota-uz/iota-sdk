352a72a4bf869d6040f7685b31205751e98d1320
Diyor Khaydarov <diyor28july@gmail.com>
2024-10-10 11:08:45 +0500

    WIP

--- internal/domain/entities/session/session_events.go
+++ internal/domain/entities/session/session_events.go
@@ -1,53 +1,23 @@
 package session
 
-import (
-	"context"
-	"github.com/iota-agency/iota-erp/internal/domain/entities/user"
-	"github.com/iota-agency/iota-erp/pkg/composables"
-)
-
-func NewCreatedEvent(ctx context.Context, data CreateDTO, result Session) (*CreatedEvent, error) {
-	sender, err := composables.UseUser(ctx)
-	if err != nil {
-		return nil, err
-	}
-	sess, err := composables.UseSession(ctx)
-	if err != nil {
-		return nil, err
-	}
+func NewCreatedEvent(data CreateDTO, result Session) (*CreatedEvent, error) {
 	return &CreatedEvent{
-		Sender:  *sender,
-		Session: *sess,
-		Data:    data,
-		Result:  result,
+		Data:   data,
+		Result: result,
 	}, nil
 }
 
-func NewDeletedEvent(ctx context.Context, result Session) (*DeletedEvent, error) {
-	sender, err := composables.UseUser(ctx)
-	if err != nil {
-		return nil, err
-	}
-	sess, err := composables.UseSession(ctx)
-	if err != nil {
-		return nil, err
-	}
+func NewDeletedEvent(result Session) (*DeletedEvent, error) {
 	return &DeletedEvent{
-		Sender:  *sender,
-		Session: *sess,
-		Result:  result,
+		Result: result,
 	}, nil
 }
 
 type CreatedEvent struct {
-	Session Session
-	Sender  user.User
-	Data    CreateDTO
-	Result  Session
+	Data   CreateDTO
+	Result Session
 }
 
 type DeletedEvent struct {
-	Session Session
-	Sender  user.User
-	Result  Session
+	Result Session
 }
