70acc4e541d0571d1b99713bcd13ff89f6174ebe
Diyor Khaydarov <diyor28july@gmail.com>
2024-11-17 23:52:42 +0500

    fix: operations as part of iota module

--- internal/modules/sidebarlinks.go
+++ internal/modules/sidebarlinks.go
@@ -2,7 +2,6 @@ package modules
 
 import (
 	"github.com/iota-agency/iota-erp/internal/domain/aggregates/user"
-	"github.com/iota-agency/iota-erp/internal/domain/entities/permission"
 	"github.com/iota-agency/iota-erp/internal/presentation/templates/icons"
 	"github.com/iota-agency/iota-erp/internal/types"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
@@ -32,46 +31,6 @@ func GetNavItems(localizer *i18n.Localizer, user *user.User) []types.NavigationI
 			Icon:        icons.CirclesThreePlus(icons.Props{Size: "20"}),
 			Children:    []types.NavigationItem{},
 			Permissions: nil,
-		},
-		{
-			Name: localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "NavigationLinks.Operations"}),
-			Href: "#",
-			Children: []types.NavigationItem{
-				{
-					Name:        localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "NavigationLinks.Employees"}),
-					Href:        "/operations/employees",
-					Permissions: []permission.Permission{permission.EmployeeRead},
-				},
-				{
-					Name:        localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "NavigationLinks.Settings"}),
-					Href:        "/settings",
-					Permissions: []permission.Permission{permission.SettingsRead},
-				},
-				{
-					Name:        localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "NavigationLinks.Projects"}),
-					Href:        "/projects",
-					Permissions: []permission.Permission{permission.ProjectRead},
-				},
-			},
-			Icon:        icons.Pulse(icons.Props{Size: "20"}),
-			Permissions: nil,
-		},
-		{
-			Name: localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "NavigationLinks.Enums"}),
-			Href: "#",
-			Icon: icons.CheckCircle(icons.Props{Size: "20"}),
-			Children: []types.NavigationItem{
-				{
-					Name:        localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "NavigationLinks.TaskTypes"}),
-					Href:        "/enums/task-types",
-					Permissions: nil,
-				},
-				{
-					Name:        localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "NavigationLinks.Positions"}),
-					Href:        "/enums/positions",
-					Permissions: nil,
-				},
-			},
 		},
 	}
 	registry := Load()
