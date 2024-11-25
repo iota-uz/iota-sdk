abe47fae3dcd0f1a9ebbbf591c74f5872f1b1de3
Diyor Khaydarov <diyor28july@gmail.com>
2024-11-25 16:43:44 +0500

    fix: profile page saving properly

--- pkg/presentation/controllers/account_controller.go
+++ pkg/presentation/controllers/account_controller.go
@@ -6,6 +6,8 @@ import (
 	"github.com/iota-agency/iota-sdk/pkg/composables"
 	"github.com/iota-agency/iota-sdk/pkg/presentation/mappers"
 	"github.com/iota-agency/iota-sdk/pkg/presentation/templates/pages/account"
+	"github.com/iota-agency/iota-sdk/pkg/services"
+	"github.com/iota-agency/iota-sdk/pkg/shared"
 	"github.com/iota-agency/iota-sdk/pkg/shared/middleware"
 	"github.com/iota-agency/iota-sdk/pkg/types"
 	"net/http"
@@ -14,14 +16,16 @@ import (
 )
 
 type AccountController struct {
-	app      application.Application
-	basePath string
+	app         application.Application
+	userService *services.UserService
+	basePath    string
 }
 
 func NewAccountController(app application.Application) application.Controller {
 	return &AccountController{
-		app:      app,
-		basePath: "/account",
+		app:         app,
+		userService: app.Service(services.UserService{}).(*services.UserService),
+		basePath:    "/account",
 	}
 }
 
@@ -29,15 +33,65 @@ func (c *AccountController) Register(r *mux.Router) {
 	router := r.PathPrefix(c.basePath).Subrouter()
 	router.Use(middleware.RequireAuthorization())
 	router.HandleFunc("", c.Get).Methods(http.MethodGet)
+	router.HandleFunc("", c.Post).Methods(http.MethodPost)
 }
 
-func (c *AccountController) Get(w http.ResponseWriter, r *http.Request) {
+func (c *AccountController) defaultProps(r *http.Request, errors map[string]string) (*account.ProfilePageProps, error) {
 	pageCtx, err := composables.UsePageCtx(
 		r,
 		types.NewPageData("Account.Meta.Index.Title", ""),
 	)
 	if err != nil {
+		return nil, err
+	}
+	nonNilErrors := make(map[string]string)
+	if errors != nil {
+		nonNilErrors = errors
+	}
+	u, err := composables.UseUser(r.Context())
+	if err != nil {
+		return nil, err
+	}
+	props := &account.ProfilePageProps{
+		PageContext: pageCtx,
+		PostPath:    c.basePath,
+		User:        mappers.UserToViewModel(u),
+		Errors:      nonNilErrors,
+	}
+	return props, nil
+}
+
+func (c *AccountController) Get(w http.ResponseWriter, r *http.Request) {
+	props, err := c.defaultProps(r, nil)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	templ.Handler(account.Index(props)).ServeHTTP(w, r)
+}
+
+func (c *AccountController) Post(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	dto := SaveAccountDTO{}
+	if err := shared.Decoder.Decode(&dto, r.Form); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	uniTranslator, ok := composables.UseUniLocalizer(r.Context())
+	if !ok {
+		http.Error(w, composables.ErrLocalizerNotFound.Error(), http.StatusInternalServerError)
+	}
+	errors, ok := dto.Ok(uniTranslator)
+	if !ok {
+		props, err := c.defaultProps(r, errors)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		templ.Handler(account.ProfileForm(props)).ServeHTTP(w, r)
 		return
 	}
 	u, err := composables.UseUser(r.Context())
@@ -45,10 +99,24 @@ func (c *AccountController) Get(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	props := &account.ProfilePageProps{
-		PageContext: pageCtx,
-		PostPath:    c.basePath,
-		User:        mappers.UserToViewModel(u),
+	entity, err := dto.ToEntity(u.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	templ.Handler(account.Index(props)).ServeHTTP(w, r)
+	if err := c.userService.Update(r.Context(), entity); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	props, err := c.defaultProps(r, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	templ.Handler(account.ProfileForm(&account.ProfilePageProps{
+		PageContext: props.PageContext,
+		User:        mappers.UserToViewModel(entity),
+		Errors:      map[string]string{},
+	})).ServeHTTP(w, r)
 }
