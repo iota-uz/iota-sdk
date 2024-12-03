5ced46dae3ae5d52a1b717ca4b59c1057655be56
Diyor Khaydarov <diyor28july@gmail.com>
2024-12-03 15:29:51 +0500

    fix: change modules.Load signature

--- modules/load.go
+++ modules/load.go
@@ -1,12 +1,9 @@
 package modules
 
 import (
-	"slices"
-
 	"github.com/iota-agency/iota-sdk/modules/finance"
 	"github.com/iota-agency/iota-sdk/modules/warehouse"
 	"github.com/iota-agency/iota-sdk/pkg/application"
-	"github.com/iota-agency/iota-sdk/pkg/configuration"
 )
 
 var (
@@ -16,14 +13,11 @@ var (
 	}
 )
 
-func Load(externalModules ...application.Module) []application.Module {
-	jsonConf := configuration.UseJsonConfig()
-	var result []application.Module
-	modules := append(BuiltInModules, externalModules...)
-	for _, module := range modules {
-		if slices.Contains(jsonConf.Modules, module.Name()) {
-			result = append(result, module)
+func Load(app application.Application, externalModules ...application.Module) error {
+	for _, module := range externalModules {
+		if err := module.Register(app); err != nil {
+			return err
 		}
 	}
-	return result
+	return nil
 }
