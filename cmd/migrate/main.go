58e6b3fd886d2562f88ef10391d567427b341fc5
Diyor Khaydarov <diyor28july@gmail.com>
2024-12-03 16:48:19 +0500

    feat: migrate command

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -1,39 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/iota-agency/iota-sdk/modules"
-	"github.com/iota-agency/iota-sdk/pkg/application/dbutils"
-	"github.com/iota-agency/iota-sdk/pkg/configuration"
-	"github.com/iota-agency/iota-sdk/pkg/server"
-	"gorm.io/gorm/logger"
-)
+import "github.com/iota-agency/iota-sdk/pkg/commands"
 
 func main() {
-	if len(os.Args) < 2 {
-		panic("expected 'up' or 'down' subcommands")
-	}
-	migration := os.Args[1]
-	db, err := dbutils.ConnectDB(configuration.Use().DBOpts, logger.Warn)
+	err := commands.Migrate()
 	if err != nil {
 		panic(err)
-	}
-	app := server.ConstructApp(db)
-	if err := modules.Load(app, modules.BuiltInModules...); err != nil {
-		panic(err)
-	}
-	switch migration {
-	case "up":
-		if err := app.RunMigrations(); err != nil {
-			panic(err)
-		}
-	case "down":
-		if err := app.RollbackMigrations(); err != nil {
-			panic(err)
-		}
-	default:
-		panic(fmt.Sprintf("unsupported command: %s\nSupported commands: 'up' or 'down'", os.Args[1]))
 	}
 }
---
+++ pkg/commands/migrate_command.go
@@ -0,0 +1,51 @@
+package commands
+
+import (
+	"errors"
+	"fmt"
+	"github.com/iota-agency/iota-sdk/modules"
+	"github.com/iota-agency/iota-sdk/pkg/application/dbutils"
+	"github.com/iota-agency/iota-sdk/pkg/configuration"
+	"github.com/iota-agency/iota-sdk/pkg/server"
+	"gorm.io/gorm/logger"
+	"os"
+)
+
+var (
+	ErrNoCommand = errors.New("expected 'up' or 'down' subcommands")
+)
+
+func Migrate() error {
+	if len(os.Args) < 2 {
+		return ErrNoCommand
+	}
+	migration := os.Args[1]
+	db, err := dbutils.ConnectDB(configuration.Use().DBOpts, logger.Warn)
+	if err != nil {
+		panic(err)
+	}
+	app := server.ConstructApp(db)
+	if err := modules.Load(app, modules.BuiltInModules...); err != nil {
+		return err
+	}
+	switch migration {
+	case "up":
+		if err := app.RunMigrations(); err != nil {
+			return err
+		}
+	case "down":
+		if err := app.RollbackMigrations(); err != nil {
+			return err
+		}
+	case "redo":
+		if err := app.RollbackMigrations(); err != nil {
+			return err
+		}
+		if err := app.RunMigrations(); err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("unsupported command: %s\nSupported commands: 'up', 'down', 'redo'", os.Args[1])
+	}
+	return nil
+}
