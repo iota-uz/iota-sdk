ea6d40bc45a5ade6c78de82311ded8a10c347cd4
Diyor Khaydarov <diyor28july@gmail.com>
2024-12-03 15:16:52 +0500

    fix: remove dashboard navlink from core

--- modules/sidebarlinks.go
+++ modules/sidebarlinks.go
@@ -4,7 +4,6 @@ import (
 	"github.com/iota-agency/iota-sdk/pkg/application"
 	"github.com/iota-agency/iota-sdk/pkg/domain/aggregates/user"
 	"github.com/iota-agency/iota-sdk/pkg/domain/entities/tab"
-	"github.com/iota-agency/iota-sdk/pkg/presentation/templates/icons"
 	"github.com/iota-agency/iota-sdk/pkg/types"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 )
@@ -63,18 +62,6 @@ func GetNavItems(
 	user *user.User,
 	tabs []*tab.Tab,
 ) ([]types.NavigationItem, []types.NavigationItem) {
-	items := []types.NavigationItem{
-		{
-			Name:        localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "NavigationLinks.Dashboard"}),
-			Href:        "/",
-			Icon:        icons.CirclesThreePlus(icons.Props{Size: "20"}),
-			Children:    []types.NavigationItem{},
-			Permissions: nil,
-		},
-	}
-	for _, n := range app.NavigationItems(localizer) {
-		items = append(items, n)
-	}
-	filtered := filterItems(items, user)
+	filtered := filterItems(app.NavigationItems(localizer), user)
 	return getEnabledNavItems(filtered, tabs), filtered
 }
