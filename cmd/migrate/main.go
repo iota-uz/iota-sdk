794a544dff5348d3cd8adac50b7f21b24a3c74da
Diyor Khaydarov <diyor28july@gmail.com>
2024-12-04 16:09:18 +0500

    fix: do not error on no migrations found

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"github.com/go-faster/errors"
-	"github.com/iota-agency/iota-sdk/pkg/application"
 	"github.com/iota-agency/iota-sdk/pkg/commands"
-	"log"
 )
 
 func main() {
 	err := commands.Migrate()
-	if errors.Is(err, application.ErrNoMigrationsFound) {
-		log.Println("No migrations found")
-		return
-	}
 	if err != nil {
 		panic(err)
 	}
