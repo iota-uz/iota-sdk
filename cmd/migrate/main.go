40bd8f082dcb3721763a9f3abc0dba193349683b
Diyor Khaydarov <diyor28july@gmail.com>
2024-12-04 16:03:52 +0500

    merge main

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "github.com/iota-agency/iota-sdk/pkg/commands"
+import (
+	"github.com/go-faster/errors"
+	"github.com/iota-agency/iota-sdk/pkg/application"
+	"github.com/iota-agency/iota-sdk/pkg/commands"
+	"log"
+)
 
 func main() {
 	err := commands.Migrate()
+	if errors.Is(err, application.ErrNoMigrationsFound) {
+		log.Println("No migrations found")
+		return
+	}
 	if err != nil {
 		panic(err)
 	}
