e4470295e7ff5b2900600ca9c843ff9d379b31cc
Diyor Khaydarov <diyor28july@gmail.com>
2024-12-10 11:47:50 +0500

    refactor: middleware

--- pkg/middleware/db_middleware.go
+++ pkg/middleware/db_middleware.go
@@ -8,16 +8,15 @@ import (
 	"net/http"
 )
 
-func Transactions(db *gorm.DB) mux.MiddlewareFunc {
+func Transactions() mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(
 			func(w http.ResponseWriter, r *http.Request) {
-				err := db.Transaction(
-					func(tx *gorm.DB) error {
-						next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), constants.TxKey, tx)))
-						return nil
-					},
-				)
+				db := r.Context().Value(constants.DBKey).(*gorm.DB)
+				err := db.Transaction(func(tx *gorm.DB) error {
+					next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), constants.TxKey, tx)))
+					return nil
+				})
 				if err != nil {
 					http.Error(w, err.Error(), http.StatusInternalServerError)
 				}
