06d88346da70b4d83d25f5cfd7093353d1430445
Diyor Khaydarov <diyor28july@gmail.com>
2024-12-10 19:06:53 +0500

    fix: login controller

--- pkg/middleware/i18n.go
+++ pkg/middleware/i18n.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"github.com/iota-agency/iota-sdk/pkg/composables"
+	"github.com/iota-agency/iota-sdk/pkg/constants"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -11,7 +12,8 @@ import (
 
 func WithLocalizer(bundle *i18n.Bundle) mux.MiddlewareFunc {
 	return ContextKeyValue(
-		"localizer", func(r *http.Request, _ http.ResponseWriter) interface{} {
+		constants.LocalizerKey,
+		func(r *http.Request, _ http.ResponseWriter) interface{} {
 			locale := composables.UseLocale(r.Context(), language.English)
 			return i18n.NewLocalizer(bundle, locale.String())
 		},
