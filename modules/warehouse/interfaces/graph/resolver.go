fbfed2660aaf76f7ae76e41bcc550b091165b74f
Diyor Khaydarov <diyor28july@gmail.com>
2024-12-17 17:57:19 +0500

    feat: small refactoring

--- modules/warehouse/interfaces/graph/resolver.go
+++ modules/warehouse/interfaces/graph/resolver.go
@@ -1,17 +1,22 @@
 package graph
 
-import "github.com/iota-agency/iota-sdk/pkg/application"
+import (
+	"github.com/iota-agency/iota-sdk/modules/warehouse/services"
+	"github.com/iota-agency/iota-sdk/pkg/application"
+)
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
-	app application.Application
+	app          application.Application
+	orderService *services.OrderService
 }
 
 func NewResolver(app application.Application) *Resolver {
 	return &Resolver{
-		app: app,
+		app:          app,
+		orderService: app.Service(services.OrderService{}).(*services.OrderService),
 	}
 }
