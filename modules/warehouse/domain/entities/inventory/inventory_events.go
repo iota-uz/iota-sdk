fbc1cd0657d7441eaa485cae9d80d3e721fd4c11
jcbbb <avaz28082000@gmail.com>
2024-12-18 16:29:10 +0500

    added events

---
+++ modules/warehouse/domain/entities/inventory/inventory_events.go
@@ -0,0 +1,79 @@
+package inventory
+
+import (
+	"context"
+
+	"github.com/iota-agency/iota-sdk/pkg/composables"
+	"github.com/iota-agency/iota-sdk/pkg/domain/aggregates/user"
+	"github.com/iota-agency/iota-sdk/pkg/domain/entities/session"
+)
+
+func NewCreatedEvent(ctx context.Context, data CreateCheckDTO, result Check) (*CreatedEvent, error) {
+	sender, err := composables.UseUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+	sess, err := composables.UseSession(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &CreatedEvent{
+		Sender:  *sender,
+		Session: *sess,
+		Data:    data,
+		Result:  result,
+	}, nil
+}
+
+func NewUpdatedEvent(ctx context.Context, data UpdateCheckDTO, result Check) (*UpdatedEvent, error) {
+	sender, err := composables.UseUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+	sess, err := composables.UseSession(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &UpdatedEvent{
+		Sender:  *sender,
+		Session: *sess,
+		Data:    data,
+		Result:  result,
+	}, nil
+}
+
+func NewDeletedEvent(ctx context.Context, result Check) (*DeletedEvent, error) {
+	sender, err := composables.UseUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+	sess, err := composables.UseSession(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &DeletedEvent{
+		Sender:  *sender,
+		Session: *sess,
+		Result:  result,
+	}, nil
+}
+
+type CreatedEvent struct {
+	Sender  user.User
+	Session session.Session
+	Data    CreateCheckDTO
+	Result  Check
+}
+
+type UpdatedEvent struct {
+	Sender  user.User
+	Session session.Session
+	Data    UpdateCheckDTO
+	Result  Check
+}
+
+type DeletedEvent struct {
+	Sender  user.User
+	Session session.Session
+	Result  Check
+}
