0fdcf5e20a335ff5f4dcbdac37758b63e1d7e70e
Diyor Khaydarov <diyor28july@gmail.com>
2024-12-31 13:15:48 +0500

    fix: transaction_repository.go & money_account_repository_test.go

--- modules/finance/persistence/transaction_repository.go
+++ modules/finance/persistence/transaction_repository.go
@@ -2,19 +2,54 @@ package persistence
 
 import (
 	"context"
-	"database/sql"
 	"errors"
 	"fmt"
-	"github.com/iota-uz/iota-sdk/pkg/utils/repo"
-	"strings"
-
 	"github.com/iota-uz/iota-sdk/modules/finance/domain/entities/transaction"
 	"github.com/iota-uz/iota-sdk/modules/finance/persistence/models"
 	"github.com/iota-uz/iota-sdk/pkg/composables"
+	"github.com/iota-uz/iota-sdk/pkg/mapping"
+	"github.com/iota-uz/iota-sdk/pkg/utils/repo"
 )
 
 var (
 	ErrTransactionNotFound = errors.New("transaction not found")
+)
+
+const (
+	transactionFindQuery = `
+		SELECT id,
+			amount,
+			origin_account_id,
+			destination_account_id,
+			transaction_date,
+			accounting_period,
+			transaction_type,
+			comment,
+			created_at
+		FROM transactions`
+	transactionCountQuery  = `SELECT COUNT(*) as count FROM transactions`
+	transactionInsertQuery = `
+		INSERT INTO transactions (
+			amount,
+			origin_account_id,
+			destination_account_id,
+			transaction_date,
+			accounting_period,
+			transaction_type,
+			comment
+		)
+		VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`
+	transactionUpdateQuery = `
+		UPDATE transactions
+		SET amount = $1,
+			origin_account_id = $2,
+			destination_account_id = $3,
+			transaction_date = $4,
+			accounting_period = $5,
+			transaction_type = $6,
+			comment = $7
+		WHERE id = $8`
+	transactionDeleteQuery = `DELETE FROM transactions WHERE id = $1`
 )
 
 type GormTransactionRepository struct{}
@@ -23,88 +58,40 @@ func NewTransactionRepository() transaction.Repository {
 	return &GormTransactionRepository{}
 }
 
-func (g *GormTransactionRepository) GetPaginated(
-	ctx context.Context, params *transaction.FindParams,
-) ([]*transaction.Transaction, error) {
-	pool, err := composables.UsePool(ctx)
-	if err != nil {
-		return nil, err
-	}
-	where, args := []string{"1 = 1"}, []interface{}{}
-	if params.ID != 0 {
-		where, args = append(where, fmt.Sprintf("ex.id = $%d", len(args)+1)), append(args, params.ID)
-	}
-
+func (g *GormTransactionRepository) GetPaginated(ctx context.Context, params *transaction.FindParams) ([]*transaction.Transaction, error) {
+	where := []string{"1 = 1"}
+	var args []interface{}
 	if params.CreatedAt.To != "" && params.CreatedAt.From != "" {
-		where, args = append(where, fmt.Sprintf("ex.created_at BETWEEN $%d and $%d", len(args)+1, len(args)+2)), append(args, params.CreatedAt.From, params.CreatedAt.To)
+		where = append(where, fmt.Sprintf("created_at BETWEEN $%d and $%d", len(where), len(where)+1))
+		args = append(args, params.CreatedAt.From, params.CreatedAt.To)
 	}
-
-	rows, err := pool.Query(ctx, `
-		SELECT id, amount, origin_account_id, destination_account_id, transaction_date, accounting_period, transaction_type, comment, created_at
-		FROM transactions
-		WHERE `+strings.Join(where, " AND ")+`
-		ORDER BY id DESC
-		`+repo.FormatLimitOffset(params.Limit, params.Offset),
-		args...,
+	q := repo.Join(
+		transactionFindQuery,
+		repo.JoinWhere(where...),
+		"ORDER BY id DESC",
+		repo.FormatLimitOffset(params.Limit, params.Offset),
 	)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-	transactions := make([]*transaction.Transaction, 0)
-	for rows.Next() {
-		var t models.Transaction
-		var comment sql.NullString
-		if err := rows.Scan(
-			&t.ID,
-			&t.Amount,
-			&t.OriginAccountID,
-			&t.DestinationAccountID,
-			&t.TransactionDate,
-			&t.AccountingPeriod,
-			&t.TransactionType,
-			&comment,
-			&t.CreatedAt,
-		); err != nil {
-			return nil, err
-		}
-		t.Comment = comment.String
-		domainTransaction, err := toDomainTransaction(&t)
-		if err != nil {
-			return nil, err
-		}
-		transactions = append(transactions, domainTransaction)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-	return transactions, nil
+	return g.queryTransactions(ctx, q, args...)
 }
 
 func (g *GormTransactionRepository) Count(ctx context.Context) (int64, error) {
-	pool, err := composables.UsePool(ctx)
+	tx, err := composables.UsePoolTx(ctx)
 	if err != nil {
 		return 0, err
 	}
 	var count int64
-	if err := pool.QueryRow(ctx, `
-		SELECT COUNT(*) as count FROM transactions
-	`).Scan(&count); err != nil {
+	if err := tx.QueryRow(ctx, transactionCountQuery).Scan(&count); err != nil {
 		return 0, err
 	}
 	return count, nil
 }
 
 func (g *GormTransactionRepository) GetAll(ctx context.Context) ([]*transaction.Transaction, error) {
-	return g.GetPaginated(ctx, &transaction.FindParams{
-		Limit: 100000,
-	})
+	return g.queryTransactions(ctx, transactionFindQuery)
 }
 
 func (g *GormTransactionRepository) GetByID(ctx context.Context, id uint) (*transaction.Transaction, error) {
-	transactions, err := g.GetPaginated(ctx, &transaction.FindParams{
-		ID: id,
-	})
+	transactions, err := g.queryTransactions(ctx, repo.Join(transactionFindQuery, "WHERE id = $1"), id)
 	if err != nil {
 		return nil, err
 	}
@@ -115,49 +102,82 @@ func (g *GormTransactionRepository) GetByID(ctx context.Context, id uint) (*tran
 }
 
 func (g *GormTransactionRepository) Create(ctx context.Context, data *transaction.Transaction) error {
+	entity := toDBTransaction(data)
 	tx, err := composables.UsePoolTx(ctx)
 	if err != nil {
 		return err
 	}
-	entity := toDBTransaction(data)
-	if err := tx.QueryRow(ctx, `
-		INSERT INTO transactions (amount, origin_account_id, destination_account_id, transaction_date, accounting_period, transaction_type, comment) 
-		VALUES ($1, $2, $3, $4, $5, $6, $7)
-	`, entity.Amount, entity.OriginAccountID, entity.DestinationAccountID, entity.TransactionDate, entity.AccountingPeriod, entity.TransactionType, entity.Comment).Scan(&data.ID); err != nil {
-		return err
+	args := []interface{}{
+		entity.Amount,
+		entity.OriginAccountID,
+		entity.DestinationAccountID,
+		entity.TransactionDate,
+		entity.AccountingPeriod,
+		entity.TransactionType,
+		entity.Comment,
 	}
-	return nil
+	return tx.QueryRow(ctx, transactionInsertQuery, args...).Scan(&data.ID)
 }
 
 func (g *GormTransactionRepository) Update(ctx context.Context, data *transaction.Transaction) error {
-	tx, err := composables.UsePoolTx(ctx)
-	if err != nil {
-		return err
+	dbTransaction := toDBTransaction(data)
+	args := []interface{}{
+		dbTransaction.Amount,
+		dbTransaction.OriginAccountID,
+		dbTransaction.DestinationAccountID,
+		dbTransaction.TransactionDate,
+		dbTransaction.AccountingPeriod,
+		dbTransaction.TransactionType,
+		dbTransaction.Comment,
+		dbTransaction.ID,
 	}
-	entity := toDBTransaction(data)
-	if _, err := tx.Exec(ctx, `
-		UPDATE transactions
-		SET
-		amount = $1, origin_account_id = $2,
-		destination_account_id = $3, transaction_date = $4,
-		accounting_period = $5, transaction_type = $6,
-		comment = $7
-		WHERE id = $8
-	`, entity.Amount, entity.OriginAccountID, entity.DestinationAccountID, entity.TransactionDate, entity.AccountingPeriod, entity.TransactionType, entity.Comment, entity.ID); err != nil {
-		return err
-	}
-	return nil
+	return g.execQuery(ctx, transactionUpdateQuery, args...)
 }
 
 func (g *GormTransactionRepository) Delete(ctx context.Context, id uint) error {
+	return g.execQuery(ctx, transactionDeleteQuery, id)
+}
+
+func (g *GormTransactionRepository) queryTransactions(ctx context.Context, query string, args ...interface{}) ([]*transaction.Transaction, error) {
+	tx, err := composables.UsePoolTx(ctx)
+	if err != nil {
+		return nil, err
+	}
+	rows, err := tx.Query(ctx, query, args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var dbRows []*models.Transaction
+	for rows.Next() {
+		r := &models.Transaction{}
+		if err := rows.Scan(
+			&r.ID,
+			&r.Amount,
+			&r.OriginAccountID,
+			&r.DestinationAccountID,
+			&r.TransactionDate,
+			&r.AccountingPeriod,
+			&r.TransactionType,
+			&r.Comment,
+			&r.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+		dbRows = append(dbRows, r)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return mapping.MapDbModels(dbRows, toDomainTransaction)
+}
+
+func (g *GormTransactionRepository) execQuery(ctx context.Context, query string, args ...interface{}) error {
 	tx, err := composables.UsePoolTx(ctx)
 	if err != nil {
 		return err
 	}
-	if _, err := tx.Exec(ctx, `
-		DELETE FROM transactions WHERE id = $1
-	`, id); err != nil {
-		return err
-	}
-	return nil
+	_, err = tx.Exec(ctx, query, args...)
+	return err
 }
