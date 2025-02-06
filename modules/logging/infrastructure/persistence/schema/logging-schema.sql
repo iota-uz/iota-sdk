f829ab7b7b211a2be54bf564e67e0a600f987895
Diyor Khaydarov <diyor28july@gmail.com>
2025-02-06 17:16:58 +0500

    fix: linting errors

---
+++ modules/logging/infrastructure/persistence/schema/logging-schema.sql
@@ -0,0 +1,34 @@
+-- +migrate Up
+
+CREATE TABLE authentication_logs
+(
+    id         SERIAL PRIMARY KEY,
+    user_id    INTEGER                  NOT NULL
+        CONSTRAINT fk_user_id REFERENCES users (id) ON DELETE CASCADE,
+    ip         VARCHAR(255)             NOT NULL,
+    user_agent VARCHAR(255)             NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE action_logs
+(
+    id         SERIAL PRIMARY KEY,
+    method     VARCHAR(255) NOT NULL,
+    path       VARCHAR(255) NOT NULL,
+    user_id    INT          REFERENCES users (id) ON DELETE SET NULL,
+    after      JSON,
+    before     JSON,
+    user_agent VARCHAR(255) NOT NULL,
+    ip         VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp
+);
+
+
+CREATE INDEX authentication_logs_user_id_idx ON authentication_logs (user_id);
+CREATE INDEX authentication_logs_created_at_idx ON authentication_logs (created_at);
+
+
+-- +migrate Down
+DROP TABLE IF EXISTS action_logs CASCADE;
+DROP TABLE IF EXISTS authentication_logs CASCADE;
+
