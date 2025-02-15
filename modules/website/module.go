d95c1a5172cae126ca2f6449f92e071b54f3b127
Diyor Khaydarov <diyor28july@gmail.com>
2025-02-15 20:02:24 +0500

    fix(ci): dead code

--- modules/website/module.go
+++ modules/website/module.go
@@ -6,8 +6,6 @@ import (
 	"github.com/iota-uz/iota-sdk/pkg/application"
 )
 
-//go:generate go run github.com/99designs/gqlgen generate
-
 ////go:embed presentation/locales/*.json
 // var localeFiles embed.FS
 
