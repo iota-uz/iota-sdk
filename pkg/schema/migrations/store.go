2ed306a3988af7f8b22aa12daf37b7af8b785c3e
twinguy <twinguy17@gmail.com>
2025-02-15 19:42:18 -0600

    refactor(schema): Remove unused migration commands
    Signed-off-by: twinguy <twinguy17@gmail.com>

--- pkg/schema/migrations/store.go
+++ pkg/schema/migrations/store.go
@@ -7,8 +7,3 @@ type Store struct {
 func NewStore(path string) (*Store, error) {
 	return &Store{path: path}, nil
 }
-
-func (s *Store) ValidateMigrations() []error {
-	// TODO: Implement validation logic
-	return nil
-}
