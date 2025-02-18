6a1f8bf9217c2b23391d49b74bfa4767c0bdd386
twinguy <twinguy17@gmail.com>
2025-02-17 21:02:20 -0600

    feat(schema): add support for database indexes
    Signed-off-by: twinguy <twinguy17@gmail.com>

--- pkg/schema/types/types.go
+++ pkg/schema/types/types.go
@@ -1,6 +1,6 @@
 package types
 
-// NodeType represents the type of AST node
+// NodeType represents the type of a schema node
 type NodeType string
 
 const (
@@ -11,25 +11,16 @@ const (
 	NodeIndex      NodeType = "INDEX"
 )
 
-// Position represents the location of a node in the source SQL
-type Position struct {
-	Line   int
-	Column int
-	File   string
-}
-
-// Node represents a single node in the schema AST
+// Node represents a node in the schema tree
 type Node struct {
 	Type     NodeType
 	Name     string
 	Children []*Node
 	Metadata map[string]interface{}
-	Pos      Position
 }
 
-// SchemaTree represents the complete database schema
+// SchemaTree represents a complete database schema
 type SchemaTree struct {
 	Root     *Node
-	Version  string
 	Metadata map[string]interface{}
 }
