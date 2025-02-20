a074137c6abbbf8e440309755ad01100d70426ac
ali <ali@iota.uz>
2025-02-20 19:05:31 +0500

    refactor: add configuration finalizers and break down configuration options

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"os"
+	"runtime/debug"
 	"time"
 
 	internalassets "github.com/iota-uz/iota-sdk/internal/assets"
@@ -23,10 +24,17 @@ import (
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	_ "github.com/lib/pq"
-	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			configuration.Use().Unload()
+			debug.PrintStack()
+			os.Exit(1)
+		}
+	}()
+
 	conf := configuration.Use()
 	logFile, logger, err := logging.FileLogger(conf.LogrusLogLevel())
 	if err != nil {
@@ -40,11 +48,11 @@ func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
-	pool, err := pgxpool.New(ctx, conf.DBOpts)
+	pool, err := pgxpool.New(ctx, conf.Database.Opts)
 	if err != nil {
 		panic(err)
 	}
-	app := application.New(pool, eventbus.NewEventPublisher(logging.ConsoleLogger(logrus.WarnLevel)))
+	app := application.New(pool, eventbus.NewEventPublisher(conf.Logger()))
 	if err := modules.Load(app, modules.BuiltInModules...); err != nil {
 		log.Fatalf("failed to load modules: %v", err)
 	}
--- pkg/configuration/environment.go
+++ pkg/configuration/environment.go
@@ -8,6 +8,8 @@ import (
 	"sync"
 	"time"
 
+	"github.com/iota-uz/iota-sdk/pkg/logging"
+
 	"github.com/caarlos0/env/v11"
 	"github.com/iota-uz/utils/fs"
 	"github.com/joho/godotenv"
@@ -17,6 +19,7 @@ import (
 var singleton = sync.OnceValue(func() *Configuration {
 	c := &Configuration{}
 	if err := c.load([]string{".env", ".env.local"}); err != nil {
+		c.Unload()
 		panic(err)
 	}
 	return c
@@ -44,37 +47,63 @@ func LoadEnv(envFiles []string) (int, error) {
 	return len(existingFiles), godotenv.Load(existingFiles...)
 }
 
+type DatabaseOptions struct {
+	Opts     string `env:"-"`
+	Name     string `env:"DB_NAME" envDefault:"iota_erp"`
+	Host     string `env:"DB_HOST" envDefault:"localhost"`
+	Port     string `env:"DB_PORT" envDefault:"5432"`
+	User     string `env:"DB_USER" envDefault:"postgres"`
+	Password string `env:"DB_PASSWORD" envDefault:"postgres"`
+}
+
+func (d *DatabaseOptions) ConnectionString() string {
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
+		d.Host, d.Port, d.User, d.Name, d.Password,
+	)
+}
+
+type GoogleOptions struct {
+	RedirectURL  string `env:"GOOGLE_REDIRECT_URL"`
+	ClientID     string `env:"GOOGLE_CLIENT_ID"`
+	ClientSecret string `env:"GOOGLE_CLIENT_SECRET"`
+}
+
+type TwilioOptions struct {
+	WebhookURL  string `env:"TWILIO_WEBHOOK_URL"`
+	AccountSID  string `env:"TWILIO_ACCOUNT_SID"`
+	AuthToken   string `env:"TWILIO_AUTH_TOKEN"`
+	PhoneNumber string `env:"TWILIO_PHONE_NUMBER"`
+}
+
 type Configuration struct {
-	DBOpts             string        `env:"-"`
-	DBName             string        `env:"DB_NAME" envDefault:"iota_erp"`
-	DBHost             string        `env:"DB_HOST" envDefault:"localhost"`
-	DBPort             string        `env:"DB_PORT" envDefault:"5432"`
-	DBUser             string        `env:"DB_USER" envDefault:"postgres"`
-	DBPassword         string        `env:"DB_PASSWORD" envDefault:"postgres"`
-	GoogleRedirectURL  string        `env:"GOOGLE_REDIRECT_URL"`
-	GoogleClientID     string        `env:"GOOGLE_CLIENT_ID"`
-	GoogleClientSecret string        `env:"GOOGLE_CLIENT_SECRET"`
-	ServerPort         int           `env:"PORT" envDefault:"3200"`
-	SessionDuration    time.Duration `env:"SESSION_DURATION" envDefault:"720h"`
-	GoAppEnvironment   string        `env:"GO_APP_ENV" envDefault:"development"`
-	SocketAddress      string        `env:"-"`
-	OpenAIKey          string        `env:"OPENAI_KEY"`
-	UploadsPath        string        `env:"UPLOADS_PATH" envDefault:"static"`
-	Domain             string        `env:"DOMAIN" envDefault:"localhost"`
-	Origin             string        `env:"ORIGIN" envDefault:"http://localhost:3200"`
-	PageSize           int           `env:"PAGE_SIZE" envDefault:"25"`
-	MaxPageSize        int           `env:"MAX_PAGE_SIZE" envDefault:"100"`
-	LogLevel           string        `env:"LOG_LEVEL" envDefault:"error"`
+	Database DatabaseOptions
+	Google   GoogleOptions
+	Twilio   TwilioOptions
+
+	ServerPort       int           `env:"PORT" envDefault:"3200"`
+	SessionDuration  time.Duration `env:"SESSION_DURATION" envDefault:"720h"`
+	GoAppEnvironment string        `env:"GO_APP_ENV" envDefault:"development"`
+	SocketAddress    string        `env:"-"`
+	OpenAIKey        string        `env:"OPENAI_KEY"`
+	UploadsPath      string        `env:"UPLOADS_PATH" envDefault:"static"`
+	Domain           string        `env:"DOMAIN" envDefault:"localhost"`
+	Origin           string        `env:"ORIGIN" envDefault:"http://localhost:3200"`
+	PageSize         int           `env:"PAGE_SIZE" envDefault:"25"`
+	MaxPageSize      int           `env:"MAX_PAGE_SIZE" envDefault:"100"`
+	LogLevel         string        `env:"LOG_LEVEL" envDefault:"error"`
 	// Session ID cookie key
 	SidCookieKey        string `env:"SID_COOKIE_KEY" envDefault:"sid"`
 	OauthStateCookieKey string `env:"OAUTH_STATE_COOKIE_KEY" envDefault:"oauthState"`
 
-	TwilioWebhookURL  string `env:"TWILIO_WEBHOOK_URL"`
-	TwilioAccountSID  string `env:"TWILIO_ACCOUNT_SID"`
-	TwilioAuthToken   string `env:"TWILIO_AUTH_TOKEN"`
-	TwilioPhoneNumber string `env:"TWILIO_PHONE_NUMBER"`
-
 	TelegramBotToken string `env:"TELEGRAM_BOT_TOKEN"`
+
+	logFile *os.File
+	logger  *logrus.Logger
+}
+
+func (c *Configuration) Logger() *logrus.Logger {
+	return c.logger
 }
 
 func (c *Configuration) LogrusLogLevel() logrus.Level {
@@ -97,6 +126,13 @@ func Use() *Configuration {
 }
 
 func (c *Configuration) load(envFiles []string) error {
+	f, logger, err := logging.FileLogger(c.LogrusLogLevel())
+	if err != nil {
+		return err
+	}
+	c.logFile = f
+	c.logger = logger
+
 	n, err := LoadEnv(envFiles)
 	if err != nil {
 		return err
@@ -111,14 +147,18 @@ func (c *Configuration) load(envFiles []string) error {
 	if err := env.Parse(c); err != nil {
 		return err
 	}
-	c.DBOpts = fmt.Sprintf(
-		"host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
-		c.DBHost, c.DBPort, c.DBUser, c.DBName, c.DBPassword,
-	)
+	c.Database.Opts = c.Database.ConnectionString()
 	if c.GoAppEnvironment == "production" {
 		c.SocketAddress = fmt.Sprintf(":%d", c.ServerPort)
 	} else {
 		c.SocketAddress = fmt.Sprintf("localhost:%d", c.ServerPort)
 	}
 	return nil
+}
+
+// unload handles a graceful shutdown.
+func (c *Configuration) Unload() {
+	if c.logFile != nil {
+		c.logFile.Close()
+	}
 }
