e031de10f09e56ebd3ecf247d097808f70498957
Diyor Khaydarov <diyor28july@gmail.com>
2025-03-02 19:13:53 +0500

    fix: failing ci checks

--- modules/hrm/infrastructure/persistence/schema/hrm-schema.sql
+++ modules/hrm/infrastructure/persistence/schema/hrm-schema.sql
@@ -1,56 +1,51 @@
-CREATE TABLE positions
-(
-    id          SERIAL PRIMARY KEY,
-    name        VARCHAR(255) NOT NULL,
-    description TEXT,
-    created_at  TIMESTAMP WITH TIME ZONE DEFAULT now(),
-    updated_at  TIMESTAMP WITH TIME ZONE DEFAULT now()
+CREATE TABLE positions (
+    id serial PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    description text,
+    created_at timestamp with time zone DEFAULT now(),
+    updated_at timestamp with time zone DEFAULT now()
 );
 
-CREATE TABLE employees
-(
-    id                 SERIAL PRIMARY KEY,
-    first_name         VARCHAR(255)  NOT NULL,
-    last_name          VARCHAR(255)  NOT NULL,
-    middle_name        VARCHAR(255),
-    email              VARCHAR(255)  NOT NULL UNIQUE,
-    phone              VARCHAR(255),
-    salary             NUMERIC(9, 2) NOT NULL,
-    salary_currency_id VARCHAR(3)    REFERENCES currencies (code) ON DELETE SET NULL,
-    hourly_rate        NUMERIC(9, 2) NOT NULL,
-    coefficient        FLOAT         NOT NULL,
-    avatar_id          INT           REFERENCES uploads (id) ON DELETE SET NULL,
-    created_at         TIMESTAMP WITH TIME ZONE DEFAULT now(),
-    updated_at         TIMESTAMP WITH TIME ZONE DEFAULT now()
+CREATE TABLE employees (
+    id serial PRIMARY KEY,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    middle_name varchar(255),
+    email varchar(255) NOT NULL UNIQUE,
+    phone varchar(255),
+    salary numeric(9, 2) NOT NULL,
+    salary_currency_id varchar(3) REFERENCES currencies (code) ON DELETE SET NULL,
+    hourly_rate numeric(9, 2) NOT NULL,
+    coefficient float NOT NULL,
+    avatar_id int REFERENCES uploads (id) ON DELETE SET NULL,
+    created_at timestamp with time zone DEFAULT now(),
+    updated_at timestamp with time zone DEFAULT now()
 );
 
-CREATE TABLE employee_positions
-(
-    employee_id INT NOT NULL REFERENCES employees (id) ON DELETE CASCADE,
-    position_id INT NOT NULL REFERENCES positions (id) ON DELETE CASCADE,
+CREATE TABLE employee_positions (
+    employee_id int NOT NULL REFERENCES employees (id) ON DELETE CASCADE,
+    position_id int NOT NULL REFERENCES positions (id) ON DELETE CASCADE,
     PRIMARY KEY (employee_id, position_id)
 );
 
-CREATE TABLE employee_meta
-(
-    employee_id        INT PRIMARY KEY NOT NULL REFERENCES employees (id) ON DELETE CASCADE,
-    primary_language   VARCHAR(255),
-    secondary_language VARCHAR(255),
-    tin                VARCHAR(255),
-    pin              VARCHAR(255),
-    notes            TEXT,
-    birth_date         DATE,
-    hire_date        DATE,
-    resignation_date DATE
+CREATE TABLE employee_meta (
+    employee_id int PRIMARY KEY NOT NULL REFERENCES employees (id) ON DELETE CASCADE,
+    primary_language varchar(255),
+    secondary_language varchar(255),
+    tin varchar(255),
+    pin varchar(255),
+    notes text,
+    birth_date date,
+    hire_date date,
+    resignation_date date
 );
 
-CREATE TABLE employee_contacts
-(
-    id          SERIAL PRIMARY KEY,
-    employee_id INT          NOT NULL REFERENCES employees (id) ON DELETE CASCADE,
-    type        VARCHAR(255) NOT NULL,
-    value       VARCHAR(255) NOT NULL,
-    created_at  TIMESTAMP WITH TIME ZONE DEFAULT now(),
-    updated_at  TIMESTAMP WITH TIME ZONE DEFAULT now()
+CREATE TABLE employee_contacts (
+    id serial PRIMARY KEY,
+    employee_id int NOT NULL REFERENCES employees (id) ON DELETE CASCADE,
+    type VARCHAR(255) NOT NULL,
+    value varchar(255) NOT NULL,
+    created_at timestamp with time zone DEFAULT now(),
+    updated_at timestamp with time zone DEFAULT now()
 );
 
