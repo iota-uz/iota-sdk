7efd3ecff70c7f73963457d78786ec747ef95d70
Diyor Khaydarov <diyor28july@gmail.com>
2025-03-08 13:31:47 +0500

    refactor: spotlight

--- pkg/spotlight/spotlight.go
+++ pkg/spotlight/spotlight.go
@@ -1,3 +1,4 @@
+// Package spotlight is a package that provides a way to show a list of items in a spotlight.
 package spotlight
 
 import (
@@ -6,6 +7,17 @@ import (
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"sort"
 )
+
+type Spotlight interface {
+	Find(localizer *i18n.Localizer, q string) []Item
+	Register(...Item)
+}
+
+type Item interface {
+	Icon() templ.Component
+	Localized(localizer *i18n.Localizer) string
+	Link() string
+}
 
 func NewItem(icon templ.Component, trKey, link string) Item {
 	return &item{
