629ce6d49c131baec176a89d965ba790f04788c5
Diyor Khaydarov <diyor28july@gmail.com>
2025-03-10 23:04:51 +0500

    fix: migrations

--- migrations/changes-1741629855.sql
+++ migrations/changes-1741629855.sql
@@ -5,15 +5,15 @@ CREATE TABLE user_groups (
 	id          SERIAL8 PRIMARY KEY,
 	name        VARCHAR(255) NOT NULL UNIQUE,
 	description TEXT,
-	created_at  TIMESTAMP DEFAULT current_timestamp(),
-	updated_at  TIMESTAMP DEFAULT current_timestamp()
+	created_at  TIMESTAMP DEFAULT now(),
+	updated_at  TIMESTAMP DEFAULT now()
 );
 
 -- Change CREATE_TABLE: group_roles
 CREATE TABLE group_roles (
 	group_id   INT8 REFERENCES user_groups (id) ON DELETE CASCADE,
 	role_id    INT8 REFERENCES roles (id) ON DELETE CASCADE,
-	created_at TIMESTAMP DEFAULT current_timestamp(),
+	created_at TIMESTAMP DEFAULT now(),
 	PRIMARY KEY (group_id, role_id)
 );
 
@@ -21,7 +21,7 @@ CREATE TABLE group_roles (
 CREATE TABLE group_users (
 	group_id   INT8 REFERENCES user_groups (id) ON DELETE CASCADE,
 	user_id    INT8 REFERENCES users (id) ON DELETE CASCADE,
-	created_at TIMESTAMP DEFAULT current_timestamp(),
+	created_at TIMESTAMP DEFAULT now(),
 	PRIMARY KEY (group_id, user_id)
 );
 
