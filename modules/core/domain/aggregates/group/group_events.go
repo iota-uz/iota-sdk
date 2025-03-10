a00a1e099d7cf5b406d355a03261651adbc06a51
Diyor Khaydarov <diyor28july@gmail.com>
2025-03-10 23:46:01 +0500

    fix: group_repository && tests

--- modules/core/domain/aggregates/group/group_events.go
+++ modules/core/domain/aggregates/group/group_events.go
@@ -6,95 +6,88 @@ import (
 	"github.com/iota-uz/iota-sdk/modules/core/domain/aggregates/user"
 )
 
-// Event represents a base structure for group events
-type Event struct {
+// CreatedEvent is triggered when a new group is created
+type CreatedEvent struct {
 	Group     Group
 	Timestamp time.Time
 	Actor     user.User
 }
 
-// CreatedEvent is triggered when a new group is created
-type CreatedEvent struct {
-	Event
-}
-
 // NewCreatedEvent creates a new group created event
 func NewCreatedEvent(group Group, actor user.User) *CreatedEvent {
 	return &CreatedEvent{
-		Event: Event{
-			Group:     group,
-			Timestamp: time.Now(),
-			Actor:     actor,
-		},
+		Group:     group,
+		Timestamp: time.Now(),
+		Actor:     actor,
 	}
 }
 
 // UpdatedEvent is triggered when a group is updated
 type UpdatedEvent struct {
-	Event
-	OldGroup Group
+	Group     Group
+	OldGroup  Group
+	Timestamp time.Time
+	Actor     user.User
 }
 
 // NewUpdatedEvent creates a new group updated event
 func NewUpdatedEvent(oldGroup, newGroup Group, actor user.User) *UpdatedEvent {
 	return &UpdatedEvent{
-		Event: Event{
-			Group:     newGroup,
-			Timestamp: time.Now(),
-			Actor:     actor,
-		},
-		OldGroup: oldGroup,
+		Group:     newGroup,
+		OldGroup:  oldGroup,
+		Timestamp: time.Now(),
+		Actor:     actor,
 	}
 }
 
 // DeletedEvent is triggered when a group is deleted
 type DeletedEvent struct {
-	Event
+	Group     Group
+	Timestamp time.Time
+	Actor     user.User
 }
 
 // NewDeletedEvent creates a new group deleted event
 func NewDeletedEvent(group Group, actor user.User) *DeletedEvent {
 	return &DeletedEvent{
-		Event: Event{
-			Group:     group,
-			Timestamp: time.Now(),
-			Actor:     actor,
-		},
+		Group:     group,
+		Timestamp: time.Now(),
+		Actor:     actor,
 	}
 }
 
 // UserAddedEvent is triggered when a user is added to a group
 type UserAddedEvent struct {
-	Event
+	Group     Group
 	AddedUser user.User
+	Timestamp time.Time
+	Actor     user.User
 }
 
 // NewUserAddedEvent creates a new user added to group event
 func NewUserAddedEvent(group Group, addedUser user.User, actor user.User) *UserAddedEvent {
 	return &UserAddedEvent{
-		Event: Event{
-			Group:     group,
-			Timestamp: time.Now(),
-			Actor:     actor,
-		},
+		Group:     group,
 		AddedUser: addedUser,
+		Timestamp: time.Now(),
+		Actor:     actor,
 	}
 }
 
 // UserRemovedEvent is triggered when a user is removed from a group
 type UserRemovedEvent struct {
-	Event
+	Group       Group
 	RemovedUser user.User
+	Timestamp   time.Time
+	Actor       user.User
 }
 
 // NewUserRemovedEvent creates a new user removed from group event
 func NewUserRemovedEvent(group Group, removedUser user.User, actor user.User) *UserRemovedEvent {
 	return &UserRemovedEvent{
-		Event: Event{
-			Group:     group,
-			Timestamp: time.Now(),
-			Actor:     actor,
-		},
+		Group:       group,
 		RemovedUser: removedUser,
+		Timestamp:   time.Now(),
+		Actor:       actor,
 	}
 }
--- modules/core/services/setup_test.go
+++ modules/core/services/setup_test.go
@@ -1,8 +1,15 @@
 package services_test
 
 import (
+	"context"
 	"os"
 	"testing"
+
+	"github.com/iota-uz/iota-sdk/modules"
+	"github.com/iota-uz/iota-sdk/pkg/application"
+	"github.com/iota-uz/iota-sdk/pkg/composables"
+	"github.com/iota-uz/iota-sdk/pkg/testutils"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func TestMain(m *testing.M) {
@@ -10,4 +17,47 @@ func TestMain(m *testing.M) {
 		panic(err)
 	}
 	os.Exit(m.Run())
+}
+
+// testFixtures contains common test dependencies
+type testFixtures struct {
+	ctx  context.Context
+	pool *pgxpool.Pool
+	app  application.Application
+}
+
+// setupTest creates all necessary dependencies for tests
+func setupTest(t *testing.T) *testFixtures {
+	t.Helper()
+
+	testutils.CreateDB(t.Name())
+	pool := testutils.NewPool(testutils.DbOpts(t.Name()))
+
+	ctx := context.Background()
+	tx, err := pool.Begin(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Cleanup(func() {
+		if err := tx.Rollback(ctx); err != nil {
+			t.Fatal(err)
+		}
+		pool.Close()
+	})
+
+	ctx = composables.WithTx(ctx, tx)
+	ctx = composables.WithParams(ctx, testutils.DefaultParams())
+
+	// Setup application and run migrations
+	app, err := testutils.SetupApplication(pool, modules.BuiltInModules...)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return &testFixtures{
+		ctx:  ctx,
+		pool: pool,
+		app:  app,
+	}
 }
