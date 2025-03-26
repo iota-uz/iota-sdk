2c7fbe5d0d7eadc66d10424886b1e39455f31f61
ali <ali@iota.uz>
2025-03-26 20:05:33 +0500

    add: tenant id to schemas

--- modules/crm/infrastructure/persistence/schema/crm-schema.sql
+++ modules/crm/infrastructure/persistence/schema/crm-schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE clients (
     id serial PRIMARY KEY,
+    tenant_id int NOT NULL REFERENCES tenants (id) ON DELETE CASCADE,
     first_name varchar(255) NOT NULL,
     last_name varchar(255),
     middle_name varchar(255),
@@ -11,7 +12,9 @@ CREATE TABLE clients (
     passport_id uuid REFERENCES passports (id) ON DELETE SET NULL ON UPDATE CASCADE,
     pin varchar(128), -- Personal Identification Number
     created_at timestamp with time zone DEFAULT now(),
-    updated_at timestamp with time zone DEFAULT now()
+    updated_at timestamp with time zone DEFAULT now(),
+    UNIQUE (tenant_id, phone_number),
+    UNIQUE (tenant_id, email)
 );
 
 CREATE TABLE client_contacts (
@@ -25,6 +28,7 @@ CREATE TABLE client_contacts (
 
 CREATE TABLE chats (
     id serial PRIMARY KEY,
+    tenant_id int NOT NULL REFERENCES tenants (id) ON DELETE CASCADE,
     created_at timestamp(3) DEFAULT now() NOT NULL,
     client_id int NOT NULL REFERENCES clients (id) ON DELETE RESTRICT ON UPDATE CASCADE,
     last_message_at timestamp(3) DEFAULT now()
@@ -50,6 +54,7 @@ CREATE TABLE message_media (
 
 CREATE TABLE message_templates (
     id serial PRIMARY KEY,
+    tenant_id int NOT NULL REFERENCES tenants (id) ON DELETE CASCADE,
     template TEXT NOT NULL,
     created_at timestamp with time zone DEFAULT now()
 );
@@ -69,6 +74,10 @@ CREATE INDEX idx_clients_last_name ON clients (last_name);
 CREATE INDEX idx_clients_phone_number ON clients (phone_number);
 
 CREATE INDEX idx_clients_email ON clients (email);
+CREATE INDEX idx_clients_tenant_id ON clients (tenant_id);
 
 CREATE INDEX idx_client_contacts_client_id ON client_contacts (client_id);
+
+CREATE INDEX idx_chats_tenant_id ON chats (tenant_id);
+CREATE INDEX idx_message_templates_tenant_id ON message_templates (tenant_id);
 
