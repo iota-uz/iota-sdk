8e61acddc074085836fa493ede23f5230c7c5d6d
ali <ali@iota.uz>
2025-03-29 19:59:42 +0500

    add: missing tenant_ids in Go code

--- modules/warehouse/domain/aggregates/order/order_impl.go
+++ modules/warehouse/domain/aggregates/order/order_impl.go
@@ -1,15 +1,17 @@
 package order
 
 import (
+	"time"
+
 	"github.com/iota-uz/iota-sdk/modules/warehouse/domain/aggregates/position"
 	"github.com/iota-uz/iota-sdk/modules/warehouse/domain/aggregates/product"
-	"time"
 )
 
 func New(orderType Type, status Status) Order {
 	return &orderImpl{
 		_type:     orderType,
 		status:    status,
+		tenantID:  0, // Will be set in repository
 		items:     make([]Item, 0),
 		createdAt: time.Now(),
 	}
@@ -18,6 +20,7 @@ func New(orderType Type, status Status) Order {
 func NewWithID(id uint, orderType Type, status Status, createdAt time.Time) Order {
 	return &orderImpl{
 		id:        id,
+		tenantID:  0, // Will be set in repository
 		_type:     orderType,
 		status:    status,
 		items:     make([]Item, 0),
@@ -27,6 +30,7 @@ func NewWithID(id uint, orderType Type, status Status, createdAt time.Time) Orde
 
 type orderImpl struct {
 	id        uint
+	tenantID  uint
 	_type     Type
 	status    Status
 	items     []Item
@@ -37,8 +41,16 @@ func (o *orderImpl) SetID(id uint) {
 	o.id = id
 }
 
+func (o *orderImpl) SetTenantID(id uint) {
+	o.tenantID = id
+}
+
 func (o *orderImpl) ID() uint {
 	return o.id
+}
+
+func (o *orderImpl) TenantID() uint {
+	return o.tenantID
 }
 
 func (o *orderImpl) Type() Type {
