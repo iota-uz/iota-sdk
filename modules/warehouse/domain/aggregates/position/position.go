8513f0e15c7802f954e5605460245b4b30b85c98
ali <ali@iota.uz>
2025-03-29 19:13:42 +0500

    add: warehouse module tenant_id

--- modules/warehouse/domain/aggregates/position/position.go
+++ modules/warehouse/domain/aggregates/position/position.go
@@ -1,13 +1,15 @@
 package position
 
 import (
+	"time"
+
 	"github.com/iota-uz/iota-sdk/modules/core/domain/entities/upload"
 	"github.com/iota-uz/iota-sdk/modules/warehouse/domain/entities/unit"
-	"time"
 )
 
 type Position struct {
 	ID        uint
+	TenantID  uint
 	Title     string
 	Barcode   string
 	UnitID    uint
