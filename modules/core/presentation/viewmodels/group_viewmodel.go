a388d714927263cc10fcceffc79608dca7aba2bd
Diyor Khaydarov <diyor28july@gmail.com>
2025-04-06 04:44:32 +0500

    fix: formatting

--- modules/core/presentation/viewmodels/group_viewmodel.go
+++ modules/core/presentation/viewmodels/group_viewmodel.go
@@ -32,15 +32,15 @@ func (g *Group) GetInitials() string {
 	if g.Name == "" {
 		return ""
 	}
-	
+
 	words := strings.Fields(g.Name)
 	if len(words) == 0 {
 		return ""
 	}
-	
+
 	if len(words) == 1 {
 		return strings.ToUpper(string(words[0][0]))
 	}
-	
+
 	return strings.ToUpper(string(words[0][0]) + string(words[len(words)-1][0]))
 }
