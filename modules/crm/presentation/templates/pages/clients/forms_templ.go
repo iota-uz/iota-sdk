7256d9e546fe12365aed90cebb8e1ae1ea120da3
danil-bragin <danil@iota.uz>
2025-04-22 01:27:28 +0500

    feat: add comments to client & gen migrations

--- modules/crm/presentation/templates/pages/clients/forms_templ.go
+++ modules/crm/presentation/templates/pages/clients/forms_templ.go
@@ -761,4 +761,57 @@ func TaxInfoEditForm(props *TaxInfoEditProps) templ.Component {
 	})
 }
 
+type NotesInfoEditProps struct {
+	Client *viewmodels.Client
+	Errors map[string]string
+	Form   string
+}
+
+func NotesInfoEditForm(props *NotesInfoEditProps) templ.Component {
+	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
+			return templ_7745c5c3_CtxErr
+		}
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+		if !templ_7745c5c3_IsBuffer {
+			defer func() {
+				templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+				if templ_7745c5c3_Err == nil {
+					templ_7745c5c3_Err = templ_7745c5c3_BufErr
+				}
+			}()
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var19 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var19 == nil {
+			templ_7745c5c3_Var19 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		pageCtx := composables.UsePageCtx(ctx)
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 25, "<div><input type=\"hidden\" name=\"section\" value=\"notes\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = input.TextArea(&input.TextAreaProps{
+			Placeholder: pageCtx.T("Clients.Single.Comments.Placeholder"),
+			Value:       props.Client.Comments,
+			Attrs: templ.Attributes{
+				"name": "Comments",
+				"rows": "3",
+				"form": props.Form,
+			},
+			Error: props.Errors["Comments"],
+		}).Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 26, "</div>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		return nil
+	})
+}
+
 var _ = templruntime.GeneratedTemplate
