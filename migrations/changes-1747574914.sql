8f47b260e084088a95c42d1c2c8b9b2e0ec04b18
danil-bragin <danil@iota.uz>
2025-05-18 20:20:41 +0500

    again pg format

--- migrations/changes-1747574914.sql
+++ migrations/changes-1747574914.sql
@@ -1,29 +1,29 @@
 -- +migrate Up
 -- Change CREATE_TABLE: billing_transactions
-create table billing_transactions (
-    id uuid default gen_random_uuid () primary key,
-    status varchar(50) not null check (status in ('created', 'pending', 'completed', 'failed', 'canceled', 'refunded', 'partially-refunded', 'expired')),
-    quantity float8 not null,
-    currency varchar(3) not null check (currency in ('UZS', 'USD', 'EUR', 'RUB')),
-    gateway varchar(50) not null check (gateway in ('click', 'payme', 'octo', 'stripe')),
-    details jsonb not null,
-    created_at timestamptz default NOW() not null,
-    updated_at timestamptz default NOW() not null
+CREATE TABLE billing_transactions (
+                                      id uuid DEFAULT gen_random_uuid () PRIMARY KEY,
+                                      status varchar(50) NOT NULL CHECK (status IN ('created', 'pending', 'completed', 'failed', 'canceled', 'refunded', 'partially-refunded', 'expired')),
+                                      quantity float8 NOT NULL,
+                                      currency varchar(3) NOT NULL CHECK (currency IN ('UZS', 'USD', 'EUR', 'RUB')),
+                                      gateway varchar(50) NOT NULL CHECK (gateway IN ('click', 'payme', 'octo', 'stripe')),
+                                      details jsonb NOT NULL,
+                                      created_at timestamptz DEFAULT NOW() NOT NULL,
+                                      updated_at timestamptz DEFAULT NOW() NOT NULL
 );
 
 -- Change CREATE_INDEX: idx_billing_transactions_gateway
-create index idx_billing_transactions_gateway on billing_transactions (gateway);
+CREATE INDEX idx_billing_transactions_gateway ON billing_transactions (gateway);
 
 -- Change CREATE_INDEX: idx_billing_transactions_status
-create index idx_billing_transactions_status on billing_transactions (status);
+CREATE INDEX idx_billing_transactions_status ON billing_transactions (status);
 
 -- +migrate Down
 -- Undo CREATE_INDEX: idx_billing_transactions_status
-drop index idx_billing_transactions_status;
+DROP INDEX idx_billing_transactions_status;
 
 -- Undo CREATE_INDEX: idx_billing_transactions_gateway
-drop index idx_billing_transactions_gateway;
+DROP INDEX idx_billing_transactions_gateway;
 
 -- Undo CREATE_TABLE: billing_transactions
-drop table if exists billing_transactions cascade;
+DROP TABLE IF EXISTS billing_transactions CASCADE;
 
