f8be8334344cf1f3710d34c3ebfd7f7acda3b38e
Diyor Khaydarov <diyor28july@gmail.com>
2025-07-04 17:12:01 +0500

    fix: generated files

--- components/base/label_templ.go
+++ components/base/label_templ.go
@@ -5,10 +5,8 @@ package base
 
 //lint:file-ignore SA4006 This context is only used if a nested component is present.
 
-import (
-	"github.com/a-h/templ"
-	templruntime "github.com/a-h/templ/runtime"
-)
+import "github.com/a-h/templ"
+import templruntime "github.com/a-h/templ/runtime"
 
 type BaseLabelProps struct {
 	Text string
