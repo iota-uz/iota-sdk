93787c00ad571de125c65789dd1654cd624d4796
Diyor Khaydarov <dkh@iota.uz>
2025-07-20 14:14:17 +0500

    fix: linting errors

--- components/import/import_page_templ.go
+++ components/import/import_page_templ.go
@@ -5,16 +5,15 @@ package importcomponents
 
 //lint:file-ignore SA4006 This context is only used if a nested component is present.
 
-import (
-	"github.com/a-h/templ"
-	templruntime "github.com/a-h/templ/runtime"
+import "github.com/a-h/templ"
+import templruntime "github.com/a-h/templ/runtime"
 
+import (
 	icons "github.com/iota-uz/icons/phosphor"
 	"github.com/iota-uz/iota-sdk/components/base/button"
 	"github.com/iota-uz/iota-sdk/components/base/card"
 	"github.com/iota-uz/iota-sdk/modules/core/presentation/templates/layouts"
 	"github.com/iota-uz/iota-sdk/pkg/composables"
-
 	importpkg "github.com/iota-uz/iota-sdk/pkg/import"
 )
 
