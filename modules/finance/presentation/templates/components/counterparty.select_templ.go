d231c4c558ffb12cc5eb0e467769970ea960b3dc
Diyor Khaydarov <dkh@iota.uz>
2025-07-20 18:26:07 +0500

    fix: generated files

--- modules/finance/presentation/templates/components/counterparty.select_templ.go
+++ modules/finance/presentation/templates/components/counterparty.select_templ.go
@@ -5,9 +5,10 @@ package components
 
 //lint:file-ignore SA4006 This context is only used if a nested component is present.
 
+import "github.com/a-h/templ"
+import templruntime "github.com/a-h/templ/runtime"
+
 import (
-	"github.com/a-h/templ"
-	templruntime "github.com/a-h/templ/runtime"
 	"github.com/iota-uz/iota-sdk/components/base"
 	"github.com/iota-uz/iota-sdk/modules/finance/presentation/viewmodels"
 )
