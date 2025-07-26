fae8487c28c570581fcd94e9377d9338345c1ac9
Diyor Khaydarov <dkh@iota.uz>
2025-07-26 13:18:58 +0500

    fix: generated files

--- components/multilang/details_view_templ.go
+++ components/multilang/details_view_templ.go
@@ -5,11 +5,11 @@ package multilang
 
 //lint:file-ignore SA4006 This context is only used if a nested component is present.
 
+import "github.com/a-h/templ"
+import templruntime "github.com/a-h/templ/runtime"
+
 import (
 	"context"
-
-	"github.com/a-h/templ"
-	templruntime "github.com/a-h/templ/runtime"
 	"github.com/iota-uz/iota-sdk/pkg/crud/models"
 )
 
