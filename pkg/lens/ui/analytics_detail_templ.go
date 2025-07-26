228cad3cb9d9fc2e562834e0a89520d2c05e355f
Diyor Khaydarov <dkh@iota.uz>
2025-07-26 11:57:53 +0500

    fix: generated files

--- pkg/lens/ui/analytics_detail_templ.go
+++ pkg/lens/ui/analytics_detail_templ.go
@@ -5,12 +5,12 @@ package ui
 
 //lint:file-ignore SA4006 This context is only used if a nested component is present.
 
+import "github.com/a-h/templ"
+import templruntime "github.com/a-h/templ/runtime"
+
 import (
 	"fmt"
 	"strings"
-
-	"github.com/a-h/templ"
-	templruntime "github.com/a-h/templ/runtime"
 
 	icons "github.com/iota-uz/icons/phosphor"
 	"github.com/iota-uz/iota-sdk/components/base/card"
