463c9f0dab2a452d0a3aef6b2611495577d3236e
islom <islom.d@iota.uz>
2025-07-30 08:35:41 +0200

    fix: scaffolding - sortable columns

--- components/scaffold/table/table_templ.go
+++ components/scaffold/table/table_templ.go
@@ -364,7 +364,7 @@ func Table(config *TableConfig) templ.Component {
 				return templ_7745c5c3_Err
 			}
 		} else {
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 10, "<div id=\"sortable-table-container\">")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 10, "<div id=\"sortable-table-container\" hx-disinherit=\"*\">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
