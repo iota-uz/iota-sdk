0129e81847c7341f21a3aef3d4c115b93bdfb33c
Diyor Khaydarov <dkh@iota.uz>
2025-08-11 10:39:45 +0500

    fix: generated files

--- components/base/avatar/avatar_templ.go
+++ components/base/avatar/avatar_templ.go
@@ -5,12 +5,11 @@ package avatar
 
 //lint:file-ignore SA4006 This context is only used if a nested component is present.
 
+import "github.com/a-h/templ"
+import templruntime "github.com/a-h/templ/runtime"
+
 import (
 	"fmt"
-
-	"github.com/a-h/templ"
-	templruntime "github.com/a-h/templ/runtime"
-
 	twmerge "github.com/Oudwins/tailwind-merge-go"
 )
 
