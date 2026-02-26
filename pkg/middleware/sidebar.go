87198dc23743ff4ec487d583ffb38596dddf7255
Diyor Khaydarov <dkh@iota.uz>
2026-02-26 20:04:00 +0500

    refactor(sdk): share sidebar tab helpers in pkg/sidebar

--- pkg/middleware/sidebar.go
+++ pkg/middleware/sidebar.go
@@ -62,33 +62,14 @@ func getEnabledNavItems(items []types.NavigationItem) []types.NavigationItem {
 	return out
 }
 
-func appendIfNotEmpty(groups *[]sidebar.TabGroup, group sidebar.TabGroup) {
-	if len(group.Items) == 0 {
-		return
-	}
-	*groups = append(*groups, group)
-}
-
-func normalizeDefaultTab(groups []sidebar.TabGroup, preferred string) string {
-	if len(groups) == 0 {
-		return ""
-	}
-	for _, group := range groups {
-		if group.Value == preferred {
-			return preferred
-		}
-	}
-	return groups[0].Value
-}
-
 func normalizeTabGroups(collection sidebar.TabGroupCollection) sidebar.TabGroupCollection {
 	groups := make([]sidebar.TabGroup, 0, len(collection.Groups))
 	for _, group := range collection.Groups {
-		appendIfNotEmpty(&groups, group)
+		pkgsidebar.AppendIfNotEmpty(&groups, group)
 	}
 	return sidebar.TabGroupCollection{
 		Groups:       groups,
-		DefaultValue: normalizeDefaultTab(groups, collection.DefaultValue),
+		DefaultValue: pkgsidebar.NormalizeDefaultTab(groups, collection.DefaultValue),
 	}
 }
 
