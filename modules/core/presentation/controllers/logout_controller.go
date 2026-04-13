98001ab7d43ae8c2d4b6305eeef0440374534cb5
Diyor Khaydarov <dkh@iota.uz>
2026-04-13 15:01:50 +0500

    fix: revoke session on logout

--- modules/core/presentation/controllers/logout_controller.go
+++ modules/core/presentation/controllers/logout_controller.go
@@ -3,11 +3,15 @@ package controllers
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/iota-uz/iota-sdk/pkg/application"
 	"github.com/iota-uz/iota-sdk/pkg/configuration"
+	"github.com/iota-uz/iota-sdk/pkg/di"
 
 	"github.com/gorilla/mux"
+	"github.com/iota-uz/iota-sdk/modules/core/services"
+	"github.com/sirupsen/logrus"
 )
 
 type LogoutController struct {
@@ -22,17 +26,41 @@ func (c *LogoutController) Key() string {
 }
 
 func (c *LogoutController) Register(r *mux.Router) {
-	r.HandleFunc("/logout", c.Logout).Methods(http.MethodGet)
+	r.HandleFunc("/logout", di.H(c.Logout)).Methods(http.MethodGet)
 }
 
-func (c *LogoutController) Logout(w http.ResponseWriter, r *http.Request) {
+func (c *LogoutController) Logout(
+	w http.ResponseWriter,
+	r *http.Request,
+	sessionService *services.SessionService,
+	logger *logrus.Entry,
+) {
 	conf := configuration.Use()
+
+	if cookie, err := r.Cookie(conf.SidCookieKey); err == nil && cookie.Value != "" {
+		if err := sessionService.Delete(r.Context(), cookie.Value); err != nil {
+			logger.WithError(err).Warn("failed to delete session on logout")
+		}
+	}
+
 	http.SetCookie(
 		w, &http.Cookie{
-			Name:   conf.SidCookieKey,
-			Value:  "",
-			MaxAge: -1,
+			Name:     conf.SidCookieKey,
+			Value:    "",
+			Domain:   conf.Domain,
+			Path:     "/",
+			MaxAge:   -1,
+			Expires:  time.Unix(0, 0),
+			HttpOnly: true,
+			SameSite: http.SameSiteLaxMode,
+			Secure:   conf.GoAppEnvironment == configuration.Production,
 		},
 	)
+
+	w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, private")
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Expires", "0")
+	w.Header().Set("Clear-Site-Data", `"cache", "cookies", "storage"`)
+
 	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
