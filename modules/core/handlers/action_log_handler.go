9ff2dc193c02641dc87cbf14487b8547d1e9cef2
Diyor Khaydarov <dkh@iota.uz>
2026-04-16 00:16:25 +0500

    complete issue 715 compatibility cleanup

--- modules/core/handlers/action_log_handler.go
+++ modules/core/handlers/action_log_handler.go
@@ -28,24 +28,5 @@ func RegisterActionLogEventHandlers(
 	return handler
 }
 
-func (h *ActionLogEventHandler) onSessionCreated(event user.CreatedEvent) {
-	//sess := event.Result
-	//logEntity := &authlog.AuthenticationLog{
-	//	ID:        0,
-	//	UserID:    sess.UserID,
-	//	IP:        sess.IP,
-	//	UserAgent: sess.UserAgent,
-	//	CreatedAt: time.Now(),
-	//}
-	//tx, err := h.pool.Begin(context.Background())
-	//if err != nil {
-	//	log.Fatalf("failed to begin transaction: %v", err)
-	//}
-	//ctx := composables.WithTx(context.Background(), tx)
-	//if err := h.authLogService.Create(ctx, logEntity); err != nil {
-	//	log.Fatalf("failed to create auth log: %v", err)
-	//}
-	//if err := tx.Commit(context.Background()); err != nil {
-	//	log.Fatalf("failed to commit transaction: %v", err)
-	//}
+func (h *ActionLogEventHandler) onSessionCreated(_ user.CreatedEvent) {
 }
