e47d783f76d9909aa474da9b0b8b19cb7c10b859
Diyor Khaydarov <dkh@iota.uz>
2026-07-26 00:13:52 +0500

    chore(lens): rebuild React distribution

--- pkg/lens/render/react/dist/assets/adapter-DkuTJb-k.js
+++ pkg/lens/render/react/dist/assets/adapter-DkuTJb-k.js
@@ -1,4 +1,4 @@
-import{i as cu,n as PS}from"./lens-dashboard-B4t3l5V8.js";/*! *****************************************************************************
+import{i as cu,n as PS}from"./lens-dashboard-uvdrC7x6.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
--- pkg/lens/render/react/dist/assets/index-CjdwGbIJ.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./adapter-CoKhagFz.js","./lens-dashboard-B4t3l5V8.js","./index-DJF72R0h.css"])))=>i.map(i=>d[i]);
-import{_ as a}from"./lens-dashboard-B4t3l5V8.js";async function o(){const{echartsAdapter:t}=await a(async()=>{const{echartsAdapter:r}=await import("./adapter-CoKhagFz.js");return{echartsAdapter:r}},__vite__mapDeps([0,1,2]),import.meta.url);return t}export{o as getChartAdapter};
