ed28290564fbef466df03d4871bf4e44dae758cf
Diyor Khaydarov <dkh@iota.uz>
2026-07-31 15:30:33 +0500

    chore(lens): rebuild React dashboard bundle

--- pkg/lens/render/react/dist/assets/adapter-DY8sOMub.js
+++ pkg/lens/render/react/dist/assets/adapter-DY8sOMub.js
@@ -1,4 +1,4 @@
-import{G as eo,H as ZS,I as Ym,J as qS,K as KS}from"./lens-dashboard-BMETE46Y.js";/*! *****************************************************************************
+import{G as eo,H as ZS,I as Ym,J as qS,K as KS}from"./lens-dashboard-CqRwNu3K.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
---
+++ pkg/lens/render/react/dist/assets/index-Z0rlWQqt.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./adapter-DY8sOMub.js","./lens-dashboard-CqRwNu3K.js","./index-DcyUy5I1.css"])))=>i.map(i=>d[i]);
+import{_ as a}from"./lens-dashboard-CqRwNu3K.js";async function o(){const{echartsAdapter:t}=await a(async()=>{const{echartsAdapter:r}=await import("./adapter-DY8sOMub.js");return{echartsAdapter:r}},__vite__mapDeps([0,1,2]),import.meta.url);return t}export{o as getChartAdapter};
