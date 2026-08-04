c92aaa344ce0cfdc60d8a7889c95ed9a1806af73
Diyor Khaydarov <dkh@iota.uz>
2026-08-04 20:50:42 +0500

    feat(lens): publish direct React package

---
+++ web/lens/src/index.ts
@@ -0,0 +1,10 @@
+export { LensDashboard, type LensDashboardProps } from './LensDashboard'
+export { LensDashboardElement, registerLensDashboardElement } from './element'
+export {
+  CONTRACT_VERSION,
+  ContractVersionMismatchError,
+  DashboardDocumentSchema,
+  parseDocument,
+  type DashboardDocument,
+} from './contract'
+export type { LensThemeMode } from './runtime'
